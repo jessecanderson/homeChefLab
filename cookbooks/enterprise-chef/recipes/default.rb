e5ead55fb60c829d01ef2f66cc02c1b9c9e0886b
Jesse C Anderson <jesse.c.anderson@icloud.com>
2016-11-06 22:32:02 -0500

    learning chef push

--- cookbooks/enterprise-chef/recipes/default.rb
+++ cookbooks/enterprise-chef/recipes/default.rb
@@ -3,3 +3,19 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+package_url = node['enterprise-chef']['url']
+package_name = ::File.basename(package_url)
+package_local_path = "#{Chef::Config[:file_cache_path]}/#{package_name}"
+
+
+# omnibus_package is remote (i.e., a URL) let's download it
+remote_file package_local_path do 
+	source package_url
+end
+
+package package_local_path
+
+# reconfigure the installation
+
+execute 'chef-server-ctl reconfigure'
