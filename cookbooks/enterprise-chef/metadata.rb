b8685ae4afe61359be365d6eae223d9a763401aa
Jesse C Anderson <jesse.c.anderson@icloud.com>
2016-11-06 22:08:20 -0500

    updated for chef learning

---
+++ cookbooks/enterprise-chef/metadata.rb
@@ -0,0 +1,15 @@
+name 'enterprise-chef'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures enterprise-chef'
+long_description 'Installs/Configures enterprise-chef'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/enterprise-chef/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/enterprise-chef' if respond_to?(:source_url)
---
+++ cookbooks/enterprise-chef/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: enterprise-chef
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
